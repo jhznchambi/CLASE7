88aa34ac8d3e78bfaf4e10e3824dcdb806c1024f
Julian <estudiante@pc2.(none)>
2026-07-22 11:42:03 -0400

    hoy hicimos tood lo relacionado con JvaScript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*const nombre = prompt("como te llamas???");
+const edad = Number(prompt("cual es tu edad???"));
+alert("hola " + nombre + " tienes " + edad);
+console.log("hola" + nombre + "tienes " + edad);
+
+function saludar(nombre2){
+    return "hola  " + nombre2;
+}
+const mensaje = saludar("dann");
+console.log(mensaje);
+const mensaje2 = saludar(prompt("cual es tu nombre"));
+console.log(mensaje2);*/
+const titulo = document.querySelector("h1");
+console.log(titulo.textContent);
+console.log(document);
+titulo.textContent = "hola clase estamos modificando desde java"
